65cf527a4910a1471a2e5492013002ee676fe358
Yashwanth <yashmy01@gmail.com>
2022-02-05 21:05:10 -0800

    Initial Commit

---
+++ components/CardItems.js
@@ -0,0 +1,11 @@
+import styles from '../styles/CardItems.module.css'
+
+const CardItems = () => {
+    return (
+        <div className={styles.carditems} >
+            <h1>Card Items</h1>
+        </div>
+    )
+}
+
+export default CardItems;
---
+++ components/Dropdown.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Dropdown.module.css'
+
+const Dropdown = () => {
+    return (
+        <div className={styles.dropdown} >
+            <h1>Dropdown</h1>
+        </div>
+    )
+}
+
+export default Dropdown;
